cc65554e45aa3ba2da2dd994c499734b819d9767
Allan Miranda Note <miranda.allan@hotmail.com>
2023-09-12 15:10:10 -0300

    att aula 06 12/09/23

---
+++ Sintaxe_Basica_JavaScript/Aula 05/script.js
@@ -0,0 +1,11 @@
+function funcao(){
+    console.log('Hello world!!!');
+}
+
+funcao();
+
+function  mensagem(primeiro, segundo){
+    console.log(primeiro, segundo);
+}
+
+mensagem('Hello', 'World!');
---
+++ Sintaxe_Basica_JavaScript/Aula 06 - Tipos de função/script.js
@@ -0,0 +1,29 @@
+
+//tipos de função
+
+//declarativas, precisa de um nome
+function funcao() {
+    console.log("Função declarativa");
+}
+funcao();
+
+//expressão de função-- nome opcional
+
+
+//com nomeação
+var funcao = function () {
+    console.log("Função de expressão");
+}
+funcao();
+
+
+//Arrow function -- não são nomeadas
+
+var funcao =()=>{
+console.log('Arrow function');
+}
+funcao();
+
+//Funções aritméticas
+
+
